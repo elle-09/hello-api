99e7433185b419ba332849be420fd9258cdaa0c5
Thwe Yamin <thweyamin@Thwes-MacBook-Air.local>
2026-09-07 11:21:24 +0700

    Add login authentication

--- my-frontend-1/src/App.jsx
+++ my-frontend-1/src/App.jsx
@@ -1,15 +1,17 @@
-//src/App.jsx
 import { HashRouter, Route, Routes } from "react-router-dom";
 import Home from "./Home";
 import Item from "./Item";
+import LoginPage from "./Login";
 
 export default function App() {
   return (
     <HashRouter>
       <Routes>
         <Route path="/" element={<Home />}>
-          <Route path="item" element={<Item />} />      
+          <Route path="item" element={<Item />} />
         </Route>
+
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
     </HashRouter>
   );
--- my-frontend-1/src/Home.jsx
+++ my-frontend-1/src/Home.jsx
@@ -6,9 +6,21 @@ import Typography from "@mui/material/Typography";
 import { Outlet } from "react-router-dom"; 
 import { useNavigate } from "react-router-dom"; 
 import Box from "@mui/material/Box"; 
+import { useContext, useEffect } from "react"; 
+import { UserContext } from "./context/UserContext"; 
+
+const API_URL = import.meta.env.VITE_API_URL; 
 
 export default function Home() { 
   const navigate = useNavigate(); 
+  const { user, isLoggedIn, isInitializing } = useContext(UserContext); 
+  useEffect(() => { 
+    if (!isLoggedIn && !isInitializing) { 
+      navigate("/login");
+    }
+  }, [isInitializing]); 
+  if (isInitializing) return <></>; 
+ 
   return ( 
     <div> 
       <AppBar position="static"> 
@@ -23,6 +35,20 @@ export default function Home() {
             }} 
           > 
             Item 
+          </Button> 
+          <Button 
+            color="inherit" 
+            onClick={async () => { 
+              const result = await fetch(`${API_URL}/api/auth/logout`, { 
+                credentials: "include", 
+              }); 
+              if (result.ok) { 
+                window.location.reload(true); 
+              } 
+
+            }} 
+          > 
+            Logout 
           </Button> 
         </Toolbar> 
       </AppBar> 
